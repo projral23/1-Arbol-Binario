6a81c63e152d96c4bcc2b9df43935e6fbfde0f9f
projral23 <rodrigolipa74@gmail.com>
2025-08-28 05:39:16 -0400

    Delete proj/controllers directory

--- proj/controllers/home_controller.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template
-
-def index():
-    data = {
-        "title": "Mi Página de Inicio",
-        "message": "Bienvenido a mi sitio web!"
-    }
-    return render_template("index.html", data=data)
